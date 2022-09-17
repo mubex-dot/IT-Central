077f88b5775f46d26cbb3f554533d10105224c5c
mubex-dot <mubee2004@gmail.com>
2022-09-17 01:18:08 +0100

    Finished project
    Added 3 models depending on which button is clicked

--- Javascript/modal.js
+++ Javascript/modal.js
@@ -1,1 +1,17 @@
-console.log("Hello world")
+function logout(){
+    document.getElementById("logout").style.display = "block"
+    document.getElementById("container").classList.add("blur")
+}
+function logoutConfirm(){
+    document.getElementById("logout").style.display = "none"
+    document.getElementById("blankpage").style.display = "block"
+}
+function logoutCancel(){
+    document.getElementById("logout").style.display = "none"
+    document.getElementById("container").classList.remove("blur")
+}
+function firstPage(){
+    document.getElementById("logout").style.display = "none"
+    document.getElementById("container").classList.remove("blur")
+    document.getElementById("blankpage").style.display = "none"
+}
